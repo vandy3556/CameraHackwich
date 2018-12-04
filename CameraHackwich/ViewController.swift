7b9822ca968c7ed6018d945c4c01e2b919db0c38
CHRISTOPHER VANDERGRIFF <rl239129@rlas-macair-38.local>
2018-12-04 10:57:10 -0600

    Initial Commit

---
+++ CameraHackwich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraHackwich
+//
+//  Created by CHRISTOPHER VANDERGRIFF on 12/4/18.
+//  Copyright © 2018 CHRISTOPHER VANDERGRIFF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
